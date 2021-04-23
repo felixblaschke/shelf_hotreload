ecebb110f33c2c6a7f5dc976b0f17a3123d64535
Felix Blaschke <felix@felix-blaschke.de>
2021-04-23 18:21:01 +0200

    initial commit

---
+++ lib/shelf_hotreload.dart
@@ -0,0 +1,7 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library shelf_hotreload;
+
+export 'src/base.dart';
+
---
+++ lib/src/base.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:hotreloader/hotreloader.dart';
+
+void withHotreload(FutureOr<HttpServer> Function() initializer) async {
+  HttpServer? runningServer;
+
+  var obtainNewServer = (FutureOr<HttpServer> newServer) async {
+    var willReplaceServer = runningServer != null;
+    await runningServer?.close(force: true);
+    runningServer = await newServer;
+
+    if (willReplaceServer) {
+      print('\nApplication reloaded.');
+    }
+  };
+
+  try {
+    await HotReloader.create(onAfterReload: (ctx) {
+      obtainNewServer(initializer());
+    });
+    print('[shelf_hotreload] Hot reload is enabled.');
+  } on StateError catch (e) {
+    if (e.message.contains('VM service not available')) {
+      print(
+          '[shelf_hotreload] Hot reload not enabled. Run this app with --enable-vm-service (or use debug run) in order to enable hot reload.');
+    } else {
+      rethrow;
+    }
+  }
+
+  await obtainNewServer(initializer());
+}
